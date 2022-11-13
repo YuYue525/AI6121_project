49630e1ee5eedcb70b7adad0853c84d7e45d8519
YuYue <58425611+YuYue525@users.noreply.github.com>
2022-11-14 02:32:54 +0800

    Create unzip_datasets.sh

---
+++ unzip_datasets.sh
@@ -0,0 +1,13 @@
+# cd AI6121_project
+
+unzip 01_images.zip -d GTA5_images
+unzip 02_images.zip -d GTA5_images
+
+unzip 01_labels.zip -d GTA5_labels
+unzip 02_labels.zip -d GTA5_labels
+
+unzip 03_images.zip -d GTA5_val_images
+unzip 03_labels.zip -d GTA5_val_labels
+
+unzip leftImg8bit_trainvaltest.zip -d Cityscapes_images
+unzip gtFine_trainvaltest.zip -d Cityscapes_labels
